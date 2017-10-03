c203b11a5493f8e50cd84451dbb44c89eb725c63
Betty <b.lam4392@gmail.com>
2017-10-02 23:26:45 -0700

    Refactored key event listener into game, board renders onto canvas depending on its content

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-  entry: './lib/game.js',
+  entry: './lib/pipe_mania.js',
   output: {
     path: path.resolve(__dirname, 'lib'),
     filename: './bundle.js',
